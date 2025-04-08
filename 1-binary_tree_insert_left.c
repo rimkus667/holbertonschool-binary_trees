80917d6b51c48c27c51339899a2d65824eb5daab
Victoire07 <victoireboutin07@gmail.com>
2025-04-08 15:02:12 +0200

    Modif2 : valeurs à initialiser Fonction qui Insère un nœud en tant qu'enfant gauche - T1K

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,19 +1,22 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_insert_left - Insère un nœud en tant qu'enfant gauche
  * @parent: Pointeur sur le nœud dans lequel insérer l'enfant gauche
  * @value: Valeur à stocker dans le nouveau noeud
+ *
+ * Return: Pointeur vers le nouveau nœud, ou NULL en cas d'échec
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = NULL;
+	binary_tree_t *new_node;
 
 	if (parent == NULL)
-	return (NULL);
+		return (NULL);
 
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
-	return (NULL);
+		return (NULL);
 
 	new_node->n = value;
 	new_node->parent = parent;
@@ -21,10 +24,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->right = NULL;
 
 	if (parent->left != NULL)
-	new_node->left = parent->left;
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
 
 	parent->left = new_node;
 
 	return (new_node);
- }
-
+}
