39af0d496b0a88817aae7973c2b1fe7c10abe12f
Victoire07 <victoireboutin07@gmail.com>
2025-04-08 15:23:31 +0200

    Q12: Compte les feuilles d'un arbre binaire

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,10 +1,16 @@
 #include "binary_trees.h"
 /**
  * binary_tree_leaves - Compte les feuilles d'un arbre binaire
- * @tree: 
- * 
+ * @tree: Pointeur vers la racine de l'arbre pour compter les feuilles
+ * Return: Nombre de feuilles, 0 si arbre NULL
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
 
+		if (tree->left == NULL && tree->right == NULL)
+			return (1);
+
+			return binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
 }
