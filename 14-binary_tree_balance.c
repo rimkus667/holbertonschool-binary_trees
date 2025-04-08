2ef1693576460e04134f5caf4d244dabccd292f3
Victoire07 <victoireboutin07@gmail.com>
2025-04-08 16:56:23 +0200

    Enregistrement des modifs légères pour merge

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - Mesure le facteur d'équilibre d'un arbre binaire
+ * @tree: Pointeur vers la racine pour mesurer le facteur d'équilibre
+ * Return: Facteur d'équilibre, ou 0 si arbre NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if(tree == NULL)
+		return (0);
+
+}
