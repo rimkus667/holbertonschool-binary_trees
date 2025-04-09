82321ef686e5f413d94dbf3a18a36581b45199c5
Victoire07 <victoireboutin07@gmail.com>
2025-04-09 20:01:16 +0200

    Update 13-binary_tree_nodes.c
    "Modification T13 pour Betty Style"

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 /**
  * binary_tree_nodes - Compte noeuds avec 1 avec au moins 1 enfants
- * @tree: pointeur sur le nœud racine de l'arbre pour compter le nombre de nœuds
+ * @tree: pointeur sur le nœud racine de l'arbre pr compter le nbr de nœuds
  * Return: Le nombres de nœuds ou 0 si arbre NULL
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
