65b5794bf3d908523dd493f0c32c860c97a81a1f
root <karimhabouchi310@gmail.com>
2025-04-09 13:00:39 +0000

    task 14 test

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,20 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree
+ * binary_tree_nodes - Count the nodes with at least 1 child in a binary tree
  * @tree: Pointer to the root node of the tree
  *
- * Return: Number of nodes with at least one child, or 0 if tree is NULL
+ * Return: Number of nodes with at least 1 child
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t count = 0;
+    if (tree == NULL)
+        return (0);
 
-	if (tree == NULL)
-		return (0);
-	if (tree->left != NULL || tree->right != NULL)
-		count = 1;
-	count += binary_tree_nodes(tree->left);
-	count += binary_tree_nodes(tree->right);
-	return (count);
+    if (tree->left != NULL || tree->right != NULL)
+        return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+
+    return (0);
 }
--- test13.c
+++ test13.c
@@ -1,12 +1,8 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+size_t binary_tree_nodes(const binary_tree_t *tree);
+
 int main(void)
 {
     binary_tree_t *root;
@@ -17,13 +13,11 @@ int main(void)
     root->right = binary_tree_node(root, 402);
     binary_tree_insert_right(root->left, 54);
     binary_tree_insert_right(root, 128);
+
     binary_tree_print(root);
 
     nodes = binary_tree_nodes(root);
     printf("Nodes in %d: %lu\n", root->n, nodes);
-    nodes = binary_tree_nodes(root->right);
-    printf("Nodes in %d: %lu\n", root->right->n, nodes);
-    nodes = binary_tree_nodes(root->left->right);
-    printf("Nodes in %d: %lu\n", root->left->right->n, nodes);
+
     return (0);
 }
