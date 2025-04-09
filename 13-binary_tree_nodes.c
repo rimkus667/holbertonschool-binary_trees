db84c3e7508c0873bc768f3ee9312ccb6f973dd4
Victoire07 <victoireboutin07@gmail.com>
2025-04-09 19:53:40 +0200

    Create 13-binary_tree_nodes.c
    "Création de la task13 suite à une reorg de la repart des exos"

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - Compte noeuds avec 1 avec au moins 1 enfants
+ * @tree: pointeur sur le nœud racine de l'arbre pour compter le nombre de nœuds
+ * Return: Le nombres de nœuds ou 0 si arbre NULL
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t compte_gauche;
+	size_t compte_droite;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+	return (0);
+
+	compte_gauche = binary_tree_nodes(tree->left);
+
+	compte_droite = binary_tree_nodes(tree->right);
+
+	return (compte_gauche + compte_droite + 1);
+
+}
